cedabd62872fe2c6945efbdce1fd866d897db65b
Ronald Lima Deveza <ronaldo45ld@gmail.com>
2026-03-13 13:01:37 -0400

    Refactor code structure and improve readability

--- analise_wei_taxas_cursos.py
+++ analise_wei_taxas_cursos.py
@@ -12,7 +12,7 @@ plt.rcParams['font.family'] = 'sans-serif'
 
 # ================= CONFIGURAÇÕES =================
 CAMINHO_PASTA = 'C:/Users/meu_endereco' #adicione aqui o endereço onde os arquivos dos microdados estão salvos
-PASTA_SAIDA = 'C:/Users/meu_endereco'   #adicione aqui o endereço onde os serão salvos
+PASTA_SAIDA = 'C:/Users/meu_endereco'   #adicione aqui o endereço onde os resultados serão salvos
 
 os.makedirs(PASTA_SAIDA, exist_ok=True)
 
